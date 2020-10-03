a22b1108ae0ae99491d46e099a2d31f733c48f28
LogaN210101 <tamojit2001@gmail.com>
2020-10-03 18:44:10 +0530

    Input modified

--- insert_data_table.py
+++ insert_data_table.py
@@ -2,8 +2,11 @@ import mysql.connector
 mydb = mysql.connector.connect(host = "localhost", user = "root", passwd = "admin", db = "python_db", port = "3308")
 
 mycursor = mydb.cursor()
+name=input("Enter Name:- ")
+phone=input("Enter phone:- ")
 
-sql = "INSERT INTO `contact_list` (`id`, `name`, `phone`) VALUES (NULL, 'Test Name', '8013576138');"
+
+sql = "INSERT INTO `contact_list` (`id`, `name`, `phone`) VALUES (NULL, '"+name+"', '"+phone+"');"
 
 mycursor.execute(sql)
 print("Inserted")
